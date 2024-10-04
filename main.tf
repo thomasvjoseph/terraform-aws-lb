98ce414061a9003f4fd0e5cda96eafb00816c2b4
thomas-jiitak <thomasvjoseph6@gmail.com>
2024-10-04 14:37:15 +0530

    changes

---
+++ main.tf
@@ -0,0 +1,83 @@
+resource "aws_lb" "load_balancer" {
+  name                        = var.lb_name
+  internal                    = var.scheme
+  load_balancer_type          = var.load_balancer_type
+  ip_address_type             = "ipv4"
+  security_groups             = var.lb_security_group
+  subnets                     = var.subnets
+  enable_deletion_protection  = var.enable_deletion_protection
+
+  tags  = { 
+    "Name"                    = var.name
+    "Env"                     = var.env
+    "Terraform"               = "true"
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name                        = var.tg_name
+  target_type                 = var.lb_target_type
+  port                        = var.tg_port_number
+  protocol                    = "HTTP"
+  vpc_id                      = var.vpc_id
+  ip_address_type             = "ipv4"
+
+  health_check {
+    path                      = var.health_check_path
+    healthy_threshold         = var.health_check_healthy_threshold
+    unhealthy_threshold       = var.health_check_unhealthy_threshold
+    timeout                   = var.health_check_timeout
+    interval                  = var.health_check_interval
+    protocol                  = var.health_check_protocol
+    matcher                   = var.health_check_matcher
+  }
+
+  tags  = {
+    "Name"                    = var.name
+    "Env"                     = var.env
+    "Terraform"               = "true"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment" {
+  count             = var.use_for == "EC2" && length(var.lb_target_id) > 0 ? 1 : 0
+  target_group_arn  = aws_lb_target_group.target_group.arn
+  target_id         = element(var.lb_target_id, 0)
+  port              = var.tg_port_number
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = var.lb_port_number
+  protocol          = var.lb_listener_protocol
+  default_action {
+    type            = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  tags  = {
+    "Name"          = var.name
+    "Env"           = var.env
+    "Terraform"     = "true"
+  }
+}
+
+resource "aws_lb_listener_rule" "listener_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+  condition {
+    path_pattern {
+      values         = ["/*"]
+    }
+  }
+
+  tags  = {
+    "Name"          = var.name
+    "Env"           = var.env
+    "Terraform"     = "true"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,125 @@
+variable "lb_name" {
+  description = "Name of the Load Balancer"
+  type        = string
+}
+
+variable "scheme" {
+  description = "Load balancer scheme (internet-facing or internal)"
+  type        = bool
+  default     = false
+}
+
+variable "lb_security_group" {
+  description = "Security groups for the load balancer"
+  type        = list(string)
+}
+
+variable "lb_target_type" {
+  description = "Target type for the load balancer"
+  type        = string
+}
+
+variable "tg_name" {
+  description = "Target group name"
+  type        = string
+}
+
+variable "tg_port_number" {
+  description = "Port number for the target group"
+  type        = number
+}
+
+variable "lb_port_number" {
+  description = "Port number for the load balancer"
+  type        = number
+}
+
+variable "lb_listener_protocol" {
+  description = "Protocol for the load balancer listener"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "lb_target_id" {
+  description = "List of target IDs"
+  type        = list(string)
+}
+
+variable "load_balancer_type" {
+  description = "Load balancer type (application or network)"
+  type        = string
+}
+
+variable "name" {
+  description = "Resource name for tagging"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment (e.g., dev, prod)"
+  type        = string
+}
+
+variable "use_for" {
+  description = "Defines whether the LB is used for EC2 or ECS"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the resources will be deployed"
+  type        = string
+}
+
+variable "subnets" {
+  description = "List of subnets where the load balancer will be deployed"
+  type        = list(string)
+}
+
+variable "enable_deletion_protection" {
+  description = "Enable deletion protection for the load balancer"
+  type        = bool
+  default     = false
+}
+
+# Health Check Optional Variables
+variable "health_check_path" {
+  description = "The destination for the health check requests"
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_healthy_threshold" {
+  description = "The number of consecutive successful health checks required before considering the target healthy"
+  type        = number
+  default     = 5
+}
+
+variable "health_check_unhealthy_threshold" {
+  description = "The number of consecutive failed health checks required before considering the target unhealthy"
+  type        = number
+  default     = 2
+}
+
+variable "health_check_timeout" {
+  description = "The time, in seconds, to wait for a response from the target before considering it unhealthy"
+  type        = number
+  default     = 5
+}
+
+variable "health_check_interval" {
+  description = "The approximate interval, in seconds, between health checks of an individual target"
+  type        = number
+  default     = 30
+}
+
+variable "health_check_protocol" {
+  description = "The protocol used when performing the health check"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "health_check_matcher" {
+  description = "The HTTP status code to match"
+  type        = string
+  default     = "200-399"
+}
